09a32d901b228e2c4e2d457fcbcdeceb734310ed
phuopham <108047552+phuopham@users.noreply.github.com>
2024-10-25 16:10:08 +0700

    change favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,7 +57,7 @@ export default defineConfig({
       },
       disable404Route: true,
       customCss: ["./src/assets/styles/starlight.css"],
-      favicon: "/favicon.ico",
+      favicon: "/bts-logo.ico",
       components: {
         SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
         Head: "./src/components/ui/starlight/Head.astro",
